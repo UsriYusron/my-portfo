c97b7e52cff48fb863715ef3746088f0320588d7
yrn.norsuy <yrn.norsuy@gmail.com>
2025-09-03 10:08:00 +0000

    edit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     allowedDevOrigins: ["https://3000-firebase-my-portfo-1756878386882.cluster-ulqnojp5endvgve6krhe7klaws.cloudworkstations.dev"],
   },
