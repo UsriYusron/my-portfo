35a2d6e8fb7ea925ff0ea9db0b29e87af0678b69
yrn.norsuy <yrn.norsuy@gmail.com>
2025-09-07 07:38:53 +0000

    update route id project

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -15,18 +15,21 @@ export async function GET(req: NextRequest, { params }: any) {
     return NextResponse.json(project);
 }
 
+// Definisikan tipe untuk context di sini
+type RouteContext = {
+    params: {
+        id: any; // Gunakan 'string' daripada 'any' untuk type safety
+    };
+};
+
 // UPDATE project
-export async function PUT(
-    req: NextRequest,
-    { params }: { params: { id: any} } // Memberi tipe yang benar
-) {
+export async function PUT(req: NextRequest, context: RouteContext) {
+    const { id } = context.params;
 
     try {
         const data = await req.json();
         const projects = await prisma.project.update({
-            where: {
-                id: params.id, // <-- LANGSUNG GUNAKAN params.id DI SINI
-            },
+            where: { id },
             data: {
                 description: data.description,
                 name: data.name,
@@ -43,8 +46,8 @@ export async function PUT(
 }
 
 // DELETE project
-export async function DELETE(req: NextRequest, { params }: any) {
-    const { id } = params;
+export async function DELETE(req: NextRequest, context: RouteContext) {
+    const { id } = context.params;
 
     try {
         await prisma.project.delete({
